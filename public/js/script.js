e39d3707251d50ffe6ad952af9079397a07e4bbe
rapinn <rapin.nar@dome.tu.ac.th>
2024-10-18 05:42:33 +0700

    Commit

--- public/js/script.js
+++ public/js/script.js
@@ -14,7 +14,7 @@ function togglePassword() {
 
 function setRole(role) {
     document.getElementById('role').value = role;
-    document.getElementById('dropdownMenuButton').innerText = role;  // Update button text
+    document.getElementById('dropdownMenuButton').innerText = role;
     validateFields();
 }
 
@@ -84,39 +84,3 @@ document.getElementById("login-btn").addEventListener("click", function () {
             console.error("Error:", error);
         });
 });
-
-// function submitLogin() {
-//     const username = document.getElementById('username').value;
-//     const password = document.getElementById('password').value;
-
-//     fetch('/api/auth', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ username, password })
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             document.getElementById('message').innerText = data.message;
-//         })
-//         .catch(error => console.error('Error:', error));
-// }
-
-
-
-// function call_REST_API_Hello() {
-//     const username = document.getElementById('username').value;
-//     const password = document.getElementById('password').value;
-
-//     const url = (
-//         'http://localhost:8080/hello?' +
-//         new URLSearchParams({ myName: username, lastName: password }).toString()
-//     );
-
-//     fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify')
-//         .then(data => {
-//             document.getElementById('message').innerText = data.message;
-//         })
-//         .catch(error => console.error('Error:', error));
-// }
